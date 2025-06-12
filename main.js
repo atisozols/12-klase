7110ef755cf8ac88111cc0d4318f7734940b861b
atisozols <atisozols5@gmail.com>
2025-06-12 12:56:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+fetch(
+  "https://data.gov.lv/dati/lv/api/3/action/datastore_search?resource_id=92ac6e57-c5a5-444e-aaca-ae90c120cc3d"
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("There has been a problem with your fetch operation:", error);
+  });
